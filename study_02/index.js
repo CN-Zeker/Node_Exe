cc09e4ae2a48af59d10093e6500fc36f36de18b6
张科 <18217477218@163.com>
2025-04-02 17:35:42 +0800

    update：momentjs

--- study_02/index.js
+++ study_02/index.js
@@ -1,8 +1,13 @@
-import sequelize from './modules/db.js';
-import { syncDB } from './modules/sync.js';
-import { addAdmin, deleteAdmin, updateAdmin } from './service/adminService.js';
-import { addStudent } from './service/studentService.js';
-import StudentData from './mock/mockStudent.js';
+// import sequelize from './modules/db.js';
+// import { syncDB } from './modules/sync.js';
+// import { addAdmin, deleteAdmin, updateAdmin } from './service/adminService.js';
+// import { addStudent } from './service/studentService.js';
+// import StudentData from './mock/mockStudent.js';
+import moment from 'moment';
+moment.locale('zh-cn');
+
+// const m = moment.utc(new Date(), ['YYYY-MM-DD HH:mm:ss'], true).fromNow();
+const m = +moment();
 
 // syncDB();
 
@@ -12,5 +17,6 @@ import StudentData from './mock/mockStudent.js';
 // updateAdmin({ id: '2', loginPassword: '18217477218' }).then((res) => {
 //   console.log(res);
 // });
-const res = addStudent(StudentData.data);
+// const res = addStudent(StudentData.data);
+console.log(m);
 // console.log(StudentData);
